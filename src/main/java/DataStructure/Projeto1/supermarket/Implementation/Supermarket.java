e1f8412782dfc8b5d7d37d208ff387283453e3ee
caio_c <caio_c_aetano@outlook.com>
2024-06-12 12:12:09 -0300

    Aulas Estrutura de Dados

---
+++ src/main/java/DataStructure/Projeto1/supermarket/Implementation/Supermarket.java
@@ -0,0 +1,12 @@
+package DataStructure.Projeto1.supermarket.Implementation;
+
+public interface Supermarket {
+
+    void add(final String item);
+
+    void print();
+
+    void delete(final int index);
+
+
+}
